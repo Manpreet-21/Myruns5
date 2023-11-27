f73cffba4d6e288006f4abf3c6019b89ba0060e5
Manpreet-21 <84259273+Manpreet-21@users.noreply.github.com>
2023-11-26 18:58:05 -0800

    push 1
    Myruns4

---
+++ Manpreet_Singh_Sarna_MyRuns2/app/src/main/java/com/example/manpreet_singh_sarna_myruns2/LocationHelper.kt
@@ -0,0 +1,14 @@
+package com.example.manpreet_singh_sarna_myruns2
+
+//This class is used for managing geographical data like latitude and longitude
+class LocationHelper {
+    var latitude = 0.0
+    var longitude = 0.0
+
+    constructor(latitude: Double, longitude: Double) {
+        this.latitude = latitude
+        this.longitude = longitude
+    }
+
+    constructor()
+}
